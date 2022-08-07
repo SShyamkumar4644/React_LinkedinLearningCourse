78dfe26fcf8c3c91bb36c75d2a95d323f19f3536
SShyamKumar <shyamng48@gmail.com>
2022-08-07 12:59:04 +0530

    added notes

--- Ex_Files_React_js_EssT/Exercise Files/Ch04/04_03/finished/react-app/src/index.js
+++ Ex_Files_React_js_EssT/Exercise Files/Ch04/04_03/finished/react-app/src/index.js
@@ -10,6 +10,16 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+// A really important JavaScript concept that we want to know 
+// about is destructuring. So this is going to help us get 
+// ready to deal with state in our application. Now, we saw 
+// how passing properties to any component, we would just 
+// do so wherever the component was rendered. On line nine 
+// here, we could add a property called library. We could 
+// set this to say react. Now, if we go back to our app.js 
+// file, instead of just hard coding this, we're going to 
+// grab this from our props object. So we could say props.library and then we should see this being rendered here in our 
+// browser. So, let's make sure that this is working. We'll say GraphQL and this should re-render that. 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- Ex_Files_React_js_EssT/Exercise Files/Ch04/04_04/finished/react-app/src/App.js
+++ Ex_Files_React_js_EssT/Exercise Files/Ch04/04_04/finished/react-app/src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import { useState } from "react";
-
+// Managing state in a React application is really important and the most modern way to handle state variables 
+// in an app is to use a function called useState.
 function App() {
   const [emotion, setEmotion] = useState("happy");
+// Well, I'm going to set it by passing a value to this useState function. Check it out. 
+// The first value now is happy. Now, how do I assign some sort of a name to this value? 
+// We know from before that we can use array destructuring to do just this. So what we're 
+// going to do here on line five is we'll create a value for a state called emotion. Then we're 
+// going to create an updating function called setEmotion. Now, when my app first renders, the state will be happy. 
 
   return (
     <div className="App">
@@ -16,7 +22,29 @@ function App() {
         Excited
       </button>
     </div>
+    // The second thing we want to do is call this setEmotion function to change the state. We need to have this respond to some sort of user input. 
+    // So I'm going to call this button sad. We're going to hit the sad button. So now I'm going to add to this an onClick method so I can change the 
+    // emotion to sad. So here we're going to use this function to call setEmotion and setEmotion will take in the new emotion, which is sad. So check 
+    // this out, as soon as I click Sad, the current emotion will be, you guessed it, sad. Emotion is our state variable. setEmotion is the function that 
+    // we're going to use to update the state
   );
 }
 
 export default App;
+
+// But what we will actually want to do here is we want to, on line five, use this value called what 
+// and we're using the what value to figure out what does useState return? When I call this state function, 
+// what is actually being returned? So what we see here, if we hit Refresh to clear out those older logs, we 
+// see an array. This array has two values. The first is an undefined value. And then the second is a function. 
+// So we want to think about this undefined value as being the state value currently when we run the app for the 
+// first time, when we render our app component. The second value here is a function that we're going to use to update the state. 
+
+// Remember that you can use this for all sorts of new emotions if you wanted to. 
+// Let's say excited. We'll set the emotion to excited. And now when I click the 
+// Excited button, the current emotion is excited. So a state variable can be a 
+// string, it can be a Boolean, it can be an object, it can be an array. But the 
+// important thing to know is that when we pass in its initial value, this is the 
+// value when our app is actually rendered to the page for the first time and then 
+// any time I call setEmotion, I'll do so with a new state and that will change the 
+// state for the entire app.
+
