f0069543c4e045ac1f651f1a8a0e4a2b3d7d9608
SShyamKumar <shyamng48@gmail.com>
2022-09-16 08:17:03 +0530

    notes added final

--- practiceapp/src/App.js
+++ practiceapp/src/App.js
@@ -1,8 +1,16 @@
+// Nesting links with React Router v6
 import "./App.css";
+import { Link, Outlet } from "react-router-dom";
 
 function Home() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/history">History</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
       <h1>My Website</h1>
     </div>
   );
@@ -11,7 +19,22 @@ function Home() {
 export function About() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="history">History</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
       <h1>About Us</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+export function History() {
+  return (
+    <div>
+      <h1>Our History</h1>
     </div>
   );
 }
@@ -19,6 +42,12 @@ export function About() {
 export function Contact() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="history">History</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
       <h1>Contact Us</h1>
     </div>
   );
--- practiceapp/src/index.js
+++ practiceapp/src/index.js
@@ -1,14 +1,17 @@
+// Nesting links with React Router v6
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { App, About, Contact } from "./App";
+import { App, About, Contact, History } from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/about" element={<About />}>
+        <Route path="history" element={<History />} />
+      </Route>
       <Route path="/contact" element={<Contact />} />
     </Routes>
   </BrowserRouter>,
